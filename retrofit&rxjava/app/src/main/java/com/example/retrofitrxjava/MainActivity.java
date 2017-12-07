a8956cb125410d638c4846218acee02daffe099c
feng li <lifeng9@lenovo.com>
2017-12-07 14:55:24 +0800

    initView

--- retrofit&rxjava/app/src/main/java/com/example/retrofitrxjava/MainActivity.java
+++ retrofit&rxjava/app/src/main/java/com/example/retrofitrxjava/MainActivity.java
@@ -3,11 +3,22 @@ package com.example.retrofitrxjava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @author Administrator Super_Feng
+ * @class 测试retrofit+rxjava的性能框架
+ * @time 2017/12/7   14:48
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+
+    private void initView() {
     }
 }
